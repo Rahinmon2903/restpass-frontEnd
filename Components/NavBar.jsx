6e1c4b6d81abd668776750486201dea985ceedeb
Rahinmon2903 <rahin2903@gmail.com>
2025-12-09 11:08:01 +0530

    Service added

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <div>
-            
+  return (
+    <nav className="w-full border-b border-black bg-white">
+      <div className="max-w-[1200px] mx-auto px-6 h-20 flex items-center justify-between">
+
+        {/* Brand */}
+        <div className="text-center">
+          <h1 className="text-2xl font-light tracking-[0.35em] text-black">
+            ZARA
+          </h1>
         </div>
-    );
+
+        {/* Links */}
+        <div className="flex items-center gap-12 text-[11px] tracking-widest uppercase text-gray-600">
+
+          <a href="/" className="hover:text-black transition">
+            Home
+          </a>
+
+         
+
+          
+
+          <a href="/login" className="hover:text-black transition">
+            Sign In
+          </a>
+
+        </div>
+
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- Pages/Register.jsx
+++ Pages/Register.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const Register = () => {
   return (

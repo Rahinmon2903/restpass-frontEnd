ea9e73daf74cda93465359fd312e32c408c2f623
Rahinmon2903 <rahin2903@gmail.com>
2025-12-11 10:52:47 +0530

    completed

--- Service/api.js
+++ Service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api =axios.create({
-    baseURL:"https://back-endcode-restpass.onrender.com"
+    baseURL:"https://back-endcode-restpass.onrender.com/api"
 });
 
 api.interceptors.request.use(
